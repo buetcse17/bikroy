9547495b0558deefe8139ef87fab73e201d5ceff
maisharahman876 <maisharahman876@gmail.com>
2020-11-03 00:03:28 +0600

    bikroy urls e include korsi product ar job app er urls

--- bikroy/urls.py
+++ bikroy/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+     path('productSection/', include('product.urls')),
+    path('jobSection/', include('job.urls'))
     #path('product', include('product.urls'))
 ]
