156a2617057c687c0501935acaf2e451a2918d71
kawshikbuet17 <kawshik.kumar.paul@gmail.com>
2020-11-30 03:52:03 +0600

    productsection jobsection design and filter

--- product/urls.py
+++ product/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [                     # kichu na dile home e thakbe
     path('', views.products, name='product'),
     path('<int:id>/',views.list, name='listProduct'),
-    path('<int:id>/<slug:product_id>/',views.displayProduct, name='DisplayProduct')
+    path('<int:id>/product_id-<int:product_id>/',views.displayProduct, name='DisplayProduct'),
+    path('<int:id>/<slug:area>/',views.listProductAreaWise, name='listProductAreaWise')
+
                # /about dile about e jabe
  ]
